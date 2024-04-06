3f8b24f3577194b5c3214d74aafd1a81b29eaa2f
Shawn Hong <hsung951027@gmail.com>
2024-04-07 00:27:26 +0900

    Time: 40 ms (70.36%), Space: 12.5 MB (78.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,9 +5,8 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-
-        
+        cache = {v: i for i, v in enumerate(nums)}
+        for i in range(len(nums)):
+            j = cache.get(target - nums[i])
+            if i != j and j is not None:
+                return [i, j]
